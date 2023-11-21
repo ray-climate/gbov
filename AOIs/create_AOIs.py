3dcf9faba5092c24c1afd406ca4a68150e5cdb2e
ray <rui.song90@gmail.com>
2023-11-21 14:47:25 +0000

    aoi

---
+++ AOIs/create_AOIs.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    create_AOIs.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        21/11/2023 14:45
