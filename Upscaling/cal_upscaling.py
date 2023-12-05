58a1c9bd34e9f75e77dfe10859e9f3db68014dd6
ray <rui.song90@gmail.com>
2023-12-05 23:55:40 +0000

    aoi

--- Upscaling/cal_upscaling.py
+++ Upscaling/cal_upscaling.py
@@ -5,12 +5,41 @@
 # @Email:       rui.song@physics.ox.ac.uk
 # @Time:        05/12/2023 22:35
 
+from datetime import datetime
 import pandas as pd
 import os
+
+# tower coordinate dic.
+tower_coordinate = {}
+tower_coordinate['Brasschaat'] = [51.3075, 4.5199, 'BRAS']
+
+def find_closest_date_file(target_date, directory):
+    """
+    Find the file in the directory with the date closest to the target date.
+    """
+    closest_file = None
+    min_diff = float('inf')
+
+    for file in os.listdir(directory):
+        # Extracting the date from the file name (assuming YYYYMMDD format after second underscore)
+        try:
+            file_date_str = file.split('_')[2]
+            file_date = datetime.strptime(file_date_str, '%Y%m%d')
+            diff = abs((target_date - file_date).days)
+
+            if diff < min_diff:
+                min_diff = diff
+                closest_file = file
+        except (IndexError, ValueError):
+            # Handle files that do not match the expected format
+            continue
+
+    return closest_file
 
 def main():
 
     tower_retrieval_dir = '../ReferenceMeasurements/OUTPUT_dhr_bhr_tocr'
+    sentinel2_dir = '/gws/nopw/j04/gbov/ruis/gbov/Sentinel2'
 
     upscale_filelist = []
     for file in os.listdir(tower_retrieval_dir):
@@ -18,19 +47,40 @@ def main():
             upscale_filelist.append(file[:-8])
             print('Site to be upscaled: ', file[:-8])
 
-    for file in upscale_filelist:
+    for tower_file in upscale_filelist:
 
-        dhr_file = os.path.join(tower_retrieval_dir, file + '_DHR.csv')
-        bhr_file = os.path.join(tower_retrieval_dir, file + '_BHR.csv')
-        tocr_file = os.path.join(tower_retrieval_dir, file + '_TOC_R.csv')
+        dhr_file = os.path.join(tower_retrieval_dir, tower_file + '_DHR.csv')
+        bhr_file = os.path.join(tower_retrieval_dir, tower_file + '_BHR.csv')
+        tocr_file = os.path.join(tower_retrieval_dir, tower_file + '_TOC_R.csv')
 
+        # extract site name from file name
+        site_name = tower_file.split('_')[2]
+        site_code = tower_coordinate[site_name][2]
+        print('Site name, site code: ', site_name, site_code)
+
+        # read values in tocr_file
         # read values in dhr_file
         dhr_data = pd.read_csv(dhr_file)
         # iterate through each row in dhr_data
         for index, row in dhr_data.iterrows():
             # print(row['datetime'], row['dhr'])
             if row['DHR'] > 0:
-                print(row['Datetime'], row['DHR'])
+                year_str = row['Datetime'].split('-')[0]
+                sentinel2_site_dir = os.path.join(sentinel2_dir, site_code, year_str)
+                print('Upscale datetime using Sentinel2 data: ', row['Datetime'], sentinel2_site_dir)
+                # file in sentinel2_site_dir has YYYYMMDD after the second underscore, find the file with the date closest to the datetime in dhr_data
+
+                # Convert the date string to a datetime object
+                row_date = datetime.strptime(row['Datetime'].replace('-', ''), '%Y%m%d')
+                closest_file = find_closest_date_file(row_date, sentinel2_site_dir)
+
+                if closest_file:
+                    print('Upscale datetime using Sentinel2 data: ', row['Datetime'],
+                          os.path.join(sentinel2_site_dir, closest_file))
+                else:
+                    print('No matching file found for', row['Datetime'])
+
+
 
 if __name__ == '__main__':
     main()
