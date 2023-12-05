57c0117ae88694793e26c155e5d41c6c3f8682a5
ray <rui.song90@gmail.com>
2023-12-05 21:38:32 +0000

    aoi

--- ReferenceMeasurements/cal_dhr_bhr.py
+++ ReferenceMeasurements/cal_dhr_bhr.py
@@ -5,7 +5,9 @@
 # @Email:       rui.song@physics.ox.ac.uk
 # @Time:        03/12/2023 21:24
 
+from timezonefinder import TimezoneFinder
 from astral.location import Location
+import pandas as pd
 import numpy as np
 import datetime
 import os.path
@@ -60,6 +62,10 @@ def cal_tower_dhr_bhr(data, start_datetime, nominal_datetime, end_datetime, lati
     data = data[data['SW_OUT'] > 30]
     print('filter valid SW_OUT', data.shape[0])
 
+    # data filtering (valid SW_DIF_ratio)
+    data = data[data['SW_DIF_ratio'] > 0]
+    print('filter valid SW_DIF_ratio', data.shape[0])
+
     ###########################
     # extract DHR information #
     ###########################
@@ -102,3 +108,193 @@ def cal_tower_dhr_bhr(data, start_datetime, nominal_datetime, end_datetime, lati
     print("BHR is calculated: %s" % BHR_value)
 
     return DHR_value, BHR_value
+
+def cal_tower_tocr(data, start_datetime, nominal_datetime, end_datetime, latitude, longitude, alpha_DHR, alpha_BHR):
+
+    # look up table.
+    I = {}
+    I[0] = [1.0, -0.997910, -0.00894619]
+    I[5] = [1.0, -0.998980, -0.00837790]
+    I[10] = [1.0, -1.00197, -0.00665391]
+    I[15] = [1.0, -1.00702, -0.00371872]
+    I[20] = [1.0, -1.01438, 0.000524714]
+    I[25] = [1.0, -1.02443, 0.00621877]
+    I[30] = [1.0, -1.03773, 0.0135606]
+    I[35] = [1.0, -1.05501, 0.0228129]
+    I[40] = [1.0, -1.07742, 0.0343240]
+    I[45] = [1.0, -1.10665, 0.0485505]
+    I[50] = [1.0, -1.14526, 0.0661051]
+    I[55] = [1.0, -1.19740, 0.0878086]
+    I[60] = [1.0, -1.27008, 0.114795]
+    I[65] = [1.0, -1.37595, 0.148698]
+    I[70] = [1.0, -1.54059, 0.191944]
+    I[75] = [1.0, -1.82419, 0.248471]
+    I[80] = [1.0, -2.40820, 0.325351]
+    I[85] = [1.0, -4.20369, 0.438371]
+    I['bhr'] = [1.0, -1.28159, 0.0802838]
+
+    szaMax = 75.  # the applied BRDF model is only valid for the maximum sza of 75.
+    szaGap = 5.
+
+    # BRDF function 1
+    def brdf_f1(sza, vza, phi):
+
+        sza = np.deg2rad(sza)
+        vza = np.deg2rad(vza)
+        phi = np.deg2rad(phi)
+
+        parameter_1 = 1. / (2. * np.pi) * ((np.pi - phi) * np.cos(phi) + np.sin(phi)) * np.tan(sza) * np.tan(vza)
+        parameter_2 = 1. / np.pi * (np.tan(sza) + np.tan(vza) + np.sqrt(
+            np.tan(sza) ** 2 + np.tan(vza) ** 2 - 2 * np.tan(sza) * np.tan(vza) * np.cos(phi)))
+
+        return (parameter_1 - parameter_2)
+
+    # BRDF function 2
+    def brdf_f2(sza, vza, phi):
+        sza = np.deg2rad(sza)
+        vza = np.deg2rad(vza)
+        phi = np.deg2rad(phi)
+
+        ci = np.arccos(np.cos(sza) * np.cos(vza) + np.sin(vza) * np.sin(sza) * np.cos(phi))
+
+        return 4. / (3. * np.pi) / (np.cos(sza) + np.cos(vza)) * (
+                    (np.pi / 2. - ci) * np.cos(ci) + np.sin(ci)) - 1. / 3.
+
+    # convert local time to UTC, in column 1.
+    tf = TimezoneFinder()
+    tz = tf.timezone_at(lng=longitude, lat=latitude)
+    tzInfo = pytz.timezone(tz)
+
+    dates_local_format = pd.to_datetime(data['Datetime'])
+    dates_local_format = [i.replace(tzinfo=tzInfo).astimezone(tzInfo) for i in dates_local_format]
+    data['Datetime_local'] = dates_local_format
+
+    # data filtering (date and time)
+    central_time = np.copy(nominal_datetime)
+    start_time = np.copy(start_datetime)
+    end_time = np.copy(end_datetime)
+
+    # drop data (in panda dataframe) with variable 'Datetime' larger than the start_time
+    print('size of raw data: ', data.shape[0])
+    data = data[(data['Datetime'] > start_datetime) & (data['Datetime'] <= end_datetime)]
+    print('filter datetime', data.shape[0])
+
+    if data.shape[0] == 0:
+        return -9999
+
+    # data filtering (solar zenith angle)
+    data = data[data['sza'] < szaMax]
+    print('filter sza', data.shape[0])
+
+    # data filtering (valid SW_IN)
+    data = data[data['SW_IN'] > 30]
+    print('filter valid SW_IN', data.shape[0])
+
+    # data filtering (valid SW_OUT)
+    data = data[data['SW_OUT'] > 30]
+    print('filter valid SW_OUT', data.shape[0])
+
+    # data filtering (valid SW_DIF_ratio)
+    data = data[data['SW_DIF_ratio'] > 0]
+    data = data[data['SW_DIF_ratio'] < 1.]
+    print('filter valid SW_DIF_ratio', data.shape[0])
+
+    ###########################
+    # extract BHR information #
+    ###########################
+
+    BHR_value = np.nan
+    BHR_beta = np.nan
+
+    while alpha_BHR >= 0.7:
+
+        data_bhr = data[data['SW_DIF_ratio'] >= alpha_BHR]
+        bhr_avg = data_bhr['SW_OUT'] / data_bhr['SW_IN']
+
+        if len(bhr_avg[bhr_avg > 0]) > 0:
+            BHR_value = np.mean(bhr_avg[bhr_avg > 0])
+            BHR_beta = np.mean(data_bhr['SW_DIF_ratio'])
+            break
+        else:
+            alpha_BHR -= 0.02
+
+    BlueSkyAlbedo = data['SW_OUT'] / data['SW_IN']
+
+    # set BHR_value to nan if alpha_BHR < 0.75
+    if (BHR_value > 0.) & (BHR_beta > 0.):
+        data['DHR_corrected'] = (BlueSkyAlbedo - BHR_beta * BHR_value) / (1 - BHR_beta)
+    else:
+        print("Not enough measurements to correct DHR")
+        data['DHR_corrected'] = BlueSkyAlbedo
+
+    print('DHR_corrected', data['DHR_corrected'])
+
+    #######################################################################################
+    # compute BRDF kernels using DHRs within the time window.
+    #######################################################################################
+
+    szaArr = np.array(range(0, int(szaMax) + 5, 5))
+
+    wangle = 2.5
+    num = len(szaArr)
+    Y = np.zeros(num)
+    X = np.zeros((szaArr.shape[0], 3))
+
+    albedo = np.asarray(data['DHR_corrected'])
+    zen = np.asarray(data['sza'])
+
+    szaArr2 = []
+    albedo_avg = {}
+    i = 0
+
+    for a in szaArr:
+        w = np.where(np.abs(zen - a) <= wangle)
+        if len(w[0]) > 0:
+            szaArr2.append(a)
+            if len(w[0]) > 1:
+                Y[i] = np.mean(albedo[w])
+                albedo_avg[a] = np.mean(albedo[w])
+            else:
+                Y[i] = albedo[w]
+                albedo_avg[a] = albedo[w]
+            X[i, 0], X[i, 1], X[i, 2] = I[a][0], I[a][1], I[a][2]
+        i += 1
+
+    try:
+        Z = np.dot(X.T, X)
+        Z = np.linalg.inv(Z)
+        K = np.dot(Z, X.T)
+        K = np.dot(K, Y)
+        print("X is:", X)
+        print("Y is:", Y)
+        print("K is:", K)
+        print("------------------------------>")
+    except:
+        print("Not enough measurements to calculate BRDF kernels")
+        return np.nan
+
+    #######################################################################################
+    # Compute TOC-R at specified time from BRDF Kernels
+    #######################################################################################
+
+    central_time_as_datetime = datetime.datetime.strptime(str(central_time), '%Y-%m-%d')
+    tocr_time = central_time_as_datetime + datetime.timedelta(hours=10)
+    tocr_time_local_format = tzInfo.localize(tocr_time)
+    tocr_time_UTC_format = tocr_time_local_format.astimezone(pytz.utc)
+
+    print('ToC-R is calculated at local time: %s' % tocr_time_local_format)
+    print('ToC-R is calculated at UTC time: %s' % tocr_time_UTC_format)
+
+    tocr_angles = pvlib.solarposition.get_solarposition(tocr_time_UTC_format, latitude, longitude)
+    sza_tocr = tocr_angles.zenith.values[0]
+
+    print("solar zenith angle at 10:00 is %s " % sza_tocr)
+    F = np.array([1, brdf_f1(sza_tocr, 0, 0), brdf_f2(sza_tocr, 0, 0)], 'f4')
+    tocr_value = np.dot(K, F)
+
+    if (tocr_value < 0) | (tocr_value > .5):
+        tocr_value = np.nan
+
+    print("ToC-R is calculated: %s" % tocr_value)
+
+    return tocr_value
--- ReferenceMeasurements/process_all_sites.py
+++ ReferenceMeasurements/process_all_sites.py
@@ -8,6 +8,7 @@
 
 This script automatically estimates Diffuse Horizontal Radiation (DHR) using reference measurements available in the
 input directory."""
+import numpy as np
 
 from cal_dhr_bhr import *
 import pandas as pd
@@ -35,7 +36,7 @@ def plot_dhr(dhr_values, dates, output_path):
     for tick in ax.xaxis.get_major_ticks() + ax.yaxis.get_major_ticks():
         tick.label1.set_fontsize(16)
 
-    ax.set_ylim([0, 0.3])
+    ax.set_ylim([0, 0.5])
     ax.set_xlabel('Date', fontsize=16)
     ax.set_ylabel('DHR', fontsize=16)
     ax.legend(fontsize=16)
@@ -64,9 +65,38 @@ def plot_bhr(dhr_values, dates, output_path):
     for tick in ax.xaxis.get_major_ticks() + ax.yaxis.get_major_ticks():
         tick.label1.set_fontsize(16)
 
-    ax.set_ylim([0, 0.3])
+    ax.set_ylim([0, 0.5])
     ax.set_xlabel('Date', fontsize=16)
     ax.set_ylabel('BHR', fontsize=16)
+    ax.legend(fontsize=16)
+    ax.set_title(output_path.split('/')[-1][:-4], fontsize=16)
+
+    plt.tight_layout()
+    plt.savefig(output_path)
+    plt.close()
+
+def plot_tocr(dhr_values, dates, output_path):
+    """
+    Plots DHR values against measurement dates.
+
+    :param dhr_values: List of DHR values.
+    :param dates: List of measurement dates in string format '%Y-%m-%d'.
+    :param output_path: Path to save the plot.
+    """
+    dates = [datetime.datetime.strptime(date, "%Y-%m-%d") for date in dates]
+
+    fig, ax = plt.subplots(figsize=(20, 8))
+    ax.scatter(dates, dhr_values, label='ToC-R', color='red', marker='+', s=80)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    ax.xaxis.set_major_locator(mdates.MonthLocator())
+    plt.xticks(rotation=45)
+
+    for tick in ax.xaxis.get_major_ticks() + ax.yaxis.get_major_ticks():
+        tick.label1.set_fontsize(16)
+
+    ax.set_ylim([0, 0.5])
+    ax.set_xlabel('Date', fontsize=16)
+    ax.set_ylabel('ToC-R', fontsize=16)
     ax.legend(fontsize=16)
     ax.set_title(output_path.split('/')[-1][:-4], fontsize=16)
 
@@ -81,7 +111,7 @@ def main():
     tower_dir = '../sites'
     filtered_rm1_dir = './FilterRawData'
     rm1_dir = './RM1_data'
-    save_dhr_dir = './OUTPUT_dhr_bhr'
+    save_dhr_dir = './OUTPUT_dhr_bhr_tocr'
 
     if not os.path.exists(save_dhr_dir):
         os.makedirs(save_dhr_dir)
@@ -90,9 +120,10 @@ def main():
     alpha_bhr = 0.85
 
     for filename in os.listdir(rm1_dir):
-        if filename.endswith('.csv'):
+        if filename.endswith('GBOV_RM01_DryRiver_001_20211231T000000Z_20221231T143000Z_070_ACR_V2.0.csv'):
 
             dhr_values, bhr_values, albedo_dates = [], [], []
+            tocr_values, tocr_dates = [], []
 
             print(f'Processing file: {filename}')
             data = pd.read_csv(f'{filtered_rm1_dir}/{filename}', delimiter=',')
@@ -107,6 +138,7 @@ def main():
             tower_dates = np.loadtxt(f'{tower_dir}/central_dates.txt', dtype=object, delimiter=",", skiprows=1)
             start_year, end_year = filename.split('_')[4][:4], filename.split('_')[5][:4]
             year_range = range(int(start_year), int(end_year) + 1)
+            csv_start_date, csv_end_date = filename.split('_')[4][:8], filename.split('_')[5][:8]
 
             for year in year_range:
                 for m, (start_mdy, nominal_mdy, end_mdy) in enumerate(tower_dates):
@@ -115,9 +147,22 @@ def main():
                     end_date = f"{year + 1 if m == len(tower_dates) - 1 else year}-{end_mdy}"
                     nominal_date = f"{year}-{nominal_mdy}"
 
+                    # check if nominal date is within the start and end date. nominal_date is YYYY-MM-DD, start and end date are YYYYMMDD
+                    if not (int(csv_start_date.replace('-', '')) <= int(nominal_date.replace('-', '')) <= int(csv_end_date.replace('-', ''))):
+                        continue
+
                     print(
                         f"Calculating albedo for {filename} using start date {start_date}, nominal date {nominal_date} "
                         f"and end date {end_date}")
+
+                    try:
+                        toc_r = cal_tower_tocr(data, start_date, nominal_date, end_date, lat, lon, alpha_dhr, alpha_bhr)
+
+                        tocr_dates.append(nominal_date)
+                        tocr_values.append(toc_r)
+
+                    except:
+                        print('NO enough data for TOC_R calculation')
 
                     try:
                         (dhr_value, bhr_value) = cal_tower_dhr_bhr(data, start_date, nominal_date, end_date, lat, lon, alpha_dhr, alpha_bhr)
@@ -128,6 +173,72 @@ def main():
 
                     except:
                         print('NO enough data for DHR BHR calculation')
+
+            # if number of valid tocr_values is less than 10, then interpolate between the first and last valid tocr_values based on tocr_dates
+            # tocr_dates is a list of strings, in the format of YYYY-MM-DD. Convert before using np.interp
+            if len(np.asarray(tocr_values)[~np.isnan(tocr_values)]) < 12:
+
+                print('Interpolating TOC_R values')
+                tocr_dates_datetime = [datetime.datetime.strptime(date, "%Y-%m-%d") for date in tocr_dates]
+                tocr_dates_datetime = mdates.date2num(tocr_dates_datetime)
+
+                # Identify indices of first and last non-NaN values
+                valid_indices = np.where(~np.isnan(tocr_values))[0]
+                first_valid_index = valid_indices[0]
+                last_valid_index = valid_indices[-1]
+                print(f'First valid index: {first_valid_index}, last valid index: {last_valid_index}')
+                print(tocr_dates[first_valid_index], tocr_dates[last_valid_index])
+                # Perform interpolation only between the first and last valid values
+                tocr_values[first_valid_index:last_valid_index + 1] = np.interp(
+                    tocr_dates_datetime[first_valid_index:last_valid_index + 1],
+                    tocr_dates_datetime[valid_indices],
+                    np.asarray(tocr_values)[valid_indices])
+
+            # if number of valid dhr_values is less than 10, then interpolate between the first and last valid dhr_values based on albedo_dates
+            # albedo_dates is a list of strings, in the format of YYYY-MM-DD. Convert before using np.interp
+            if len(np.asarray(dhr_values)[~np.isnan(dhr_values)]) < 12:
+
+                    print('Interpolating DHR values')
+                    albedo_dates_datetime = [datetime.datetime.strptime(date, "%Y-%m-%d") for date in albedo_dates]
+                    albedo_dates_datetime = mdates.date2num(albedo_dates_datetime)
+
+                    # Identify indices of first and last non-NaN values
+                    valid_indices = np.where(~np.isnan(dhr_values))[0]
+                    first_valid_index = valid_indices[0]
+                    last_valid_index = valid_indices[-1]
+                    print(f'First valid index: {first_valid_index}, last valid index: {last_valid_index}')
+                    print(albedo_dates[first_valid_index], albedo_dates[last_valid_index])
+                    # Perform interpolation only between the first and last valid values
+                    dhr_values[first_valid_index:last_valid_index + 1] = np.interp(
+                        albedo_dates_datetime[first_valid_index:last_valid_index + 1],
+                        albedo_dates_datetime[valid_indices],
+                        np.asarray(dhr_values)[valid_indices])
+
+            # if number of valid bhr_values is less than 10, then interpolate between the first and last valid bhr_values based on albedo_dates
+            # albedo_dates is a list of strings, in the format of YYYY-MM-DD. Convert before using np.interp
+            if len(np.asarray(bhr_values)[~np.isnan(bhr_values)]) < 12:
+
+                        print('Interpolating BHR values')
+                        albedo_dates_datetime = [datetime.datetime.strptime(date, "%Y-%m-%d") for date in albedo_dates]
+                        albedo_dates_datetime = mdates.date2num(albedo_dates_datetime)
+
+                        # Identify indices of first and last non-NaN values
+                        valid_indices = np.where(~np.isnan(bhr_values))[0]
+                        first_valid_index = valid_indices[0]
+                        last_valid_index = valid_indices[-1]
+                        print(f'First valid index: {first_valid_index}, last valid index: {last_valid_index}')
+                        print(albedo_dates[first_valid_index], albedo_dates[last_valid_index])
+                        # Perform interpolation only between the first and last valid values
+                        bhr_values[first_valid_index:last_valid_index + 1] = np.interp(
+                            albedo_dates_datetime[first_valid_index:last_valid_index + 1],
+                            albedo_dates_datetime[valid_indices],
+                            np.asarray(bhr_values)[valid_indices])
+
+            with open(f'{save_dhr_dir}/{filename[:-4]}_TOC_R.csv', 'w', newline='') as output:
+                writer = csv.writer(output, lineterminator='\n')
+                writer.writerow(('Datetime', 'TOC_R'))
+                for i in range(len(tocr_dates)):
+                    writer.writerow((tocr_dates[i], tocr_values[i]))
 
             with open(f'{save_dhr_dir}/{filename[:-4]}_DHR.csv', 'w', newline='') as output:
                 writer = csv.writer(output, lineterminator='\n')
@@ -143,6 +254,7 @@ def main():
 
             plot_dhr(dhr_values, albedo_dates, f'{save_dhr_dir}/{filename[:-4]}_DHR.png')
             plot_bhr(bhr_values, albedo_dates, f'{save_dhr_dir}/{filename[:-4]}_BHR.png')
+            plot_tocr(tocr_values, tocr_dates, f'{save_dhr_dir}/{filename[:-4]}_TOC_R.png')
 
 
 if __name__ == "__main__":
