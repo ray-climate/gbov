eea3d993e817ece0838318a91944e998488db8b2
ray <rui.song90@gmail.com>
2023-12-08 10:54:29 +0000

    aoi

--- ReferenceMeasurements/process_all_sites.py
+++ ReferenceMeasurements/process_all_sites.py
@@ -118,8 +118,9 @@ def main():
 
     alpha_dhr = 0.15
     alpha_bhr = 0.85
-    print('Start processing all sites'')
+    print('Start processing all sites')
     for filename in os.listdir(rm1_dir):
+        print(filename)
         if filename.endswith('GBOV_RM01_Calperum_001_20211231T000000Z_20221231T140000Z_008_ACR_V2.0.csv'):
 
             dhr_values, bhr_values, albedo_dates = [], [], []
