9c23eb03dd947522ae1a5d6a6de697e6c0bf9de1
ray <rui.song90@gmail.com>
2023-12-08 11:02:59 +0000

    aoi

--- ReferenceMeasurements/process_all_sites.py
+++ ReferenceMeasurements/process_all_sites.py
@@ -118,9 +118,9 @@ def main():
 
     alpha_dhr = 0.15
     alpha_bhr = 0.85
-
-    for filename in os.listdir(filtered_rm1_dir):
-
+    print('Start processing all sites')
+    for filename in os.listdir(rm1_dir):
+        print(filename)
         if filename.endswith('GBOV_RM01_Calperum_001_20211231T000000Z_20221231T140000Z_008_ACR_V2.0.csv'):
 
             dhr_values, bhr_values, albedo_dates = [], [], []
@@ -131,7 +131,7 @@ def main():
             data['Datetime'] = pd.to_datetime(data['Datetime'])
             data[['SW_IN', 'SW_OUT', 'SW_DIF']] = data[['SW_IN', 'SW_OUT', 'SW_DIF']].apply(pd.to_numeric, errors='coerce')
 
-            raw_data = pd.read_csv(f'{filtered_rm1_dir}/{filename}', delimiter=';')
+            raw_data = pd.read_csv(f'{rm1_dir}/{filename}', delimiter=';')
             lat, lon = raw_data.at[0, 'Lat_IS'], raw_data.at[0, 'Lon_IS']
             data['sza'] = get_sza(data['Datetime'], lat, lon)
             data['SW_DIF_ratio'] = data['SW_DIF'] / data['SW_IN']
