e78d0579d3762242938967f27a8fbf9bcea88904
ray <rui.song90@gmail.com>
2023-12-11 16:13:08 +0000

    aoi

--- Upscaling/cal_upscaling.py
+++ Upscaling/cal_upscaling.py
@@ -32,6 +32,7 @@ tower_coordinate['Izana'] = [28.30935, -16.49926, 'IZAN']
 tower_coordinate['Grignon'] = [48.8442192077637, 1.9519100189209, 'GRIG']
 tower_coordinate['GreatWesternWoodland'] = [-30.1913, 120.6541, 'GREA']
 tower_coordinate['GoodwinCreek'] = [34.255, -89.873, 'GOOD']
+tower_coordinate['FortPeck'] = [48.308, -105.102, 'FORT']
 
 # instrument and canopy height.
 canopy_height = {}
@@ -49,6 +50,7 @@ canopy_height['Izana'] = [2., 0., 'IZAN']
 canopy_height['Grignon'] = [2., 0., 'GRIG']
 canopy_height['GreatWesternWoodland'] = [35., 0., 'GREA']
 canopy_height['GoodwinCreek'] = [10., 0., 'GOOD']
+canopy_height['FortPeck'] = [10., 0., 'FORT']
 
 # a function to generate RGB quicklook image for each site using blue (B2), green (B3), red (B4) bands.
 def create_rgb_quicklook(band2, band3, band4, output_file):
@@ -376,7 +378,7 @@ def main():
 
     upscale_filelist = []
     for file in os.listdir(tower_retrieval_dir):
-        if file.endswith('GBOV_RM01_GoodwinCreek_001_20211231T000000Z_20221231T235900Z_049_ACR_V2.0_DHR.csv'):
+        if file.endswith('GBOV_RM01_FortPeck_001_20211231T000000Z_20221230T235900Z_048_ACR_V2.0_DHR.csv'):
             upscale_filelist.append(file[:-8])
             print('Site to be upscaled: ', file[:-8])
 
